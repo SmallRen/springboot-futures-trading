a62c5523146a0ed23bd2d19ffe58495a455c12ec
renbaojia <1406423298@qq.com>
2019-03-22 23:43:20 +0800

    Initial commit

---
+++ src/main/java/com/futurestrading/FuturesTradingApplication.java
@@ -0,0 +1,14 @@
+package com.futurestrading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class FuturesTradingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FuturesTradingApplication.class, args);
+    }
+
+}
